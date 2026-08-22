a95938aaaa879a8759b1174e7f9695fa836621c7
seproispogi-lgtm <serpoispogi@email.com>
2026-08-22 21:30:38 +0800

    added the addressors &

--- calculator.c
+++ calculator.c
@@ -28,26 +28,28 @@ int main(){
 
         else if(choice == 6){
             printf("Enter your Number: ");
-            scanf("%.2f",num);
+            scanf("%.2f",&num);
             printf("%.2f", sqrt(num));
         }
-        else if (choice == 1 || 2 || 3 || 4 || 5){
-            printf("Enter your first number: ");
-            scanf("%.2f", num1);
-            printf("Enter your second number: ");
-            scanf("%.2f", num2);
+        else if (choice == 1 || choice == 2 || choice == 3 || choice == 4 || choice == 5){
+            printf("Enter your first number: \n");
+            scanf("%.2f", &num1);   
+            printf("Enter your second number: \n");
+            scanf("%.2f", &num2);
 
             if (choice == 1){printf("%.2f", num1+num2);}
             else if(choice == 2){printf("%.2f", num1-num2);}
             else if(choice == 3){printf("%.2f", num1*num2);}
             else if(choice == 4){printf("%.2f", num1/num2);}
             else if(choice == 5){float result = pow(num1, num2); printf("%.2f", result);}
-        else;
-        printf("Invalid input! Select Another");
-        
-        char again;
-        printf("Want to Continue[Y/N]: ");
-        scanf("%c", toupper(again));
+
+            else {
+            printf("Invalid input! Select Another\n");
+        }
+
+        char again = toupper(again);
+        printf("Want to Continue[Y/N]: \n");
+        scanf("%c", toupper(&again));
         
         if (again != 'Y'){
             printf("Comeagain");
