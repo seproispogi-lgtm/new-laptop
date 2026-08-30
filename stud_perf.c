79b5488925e90e649861b3568fdc15dc0224c452
seproispogi-lgtm <serpoispogi@email.com>
2026-08-30 09:47:26 +0800

    fixing errors, in for loop  statement

--- stud_perf.c
+++ stud_perf.c
@@ -23,35 +23,32 @@ int main(){
                 failCount = 0;
         
         for(currentStudent = 0; currentStudent < studentCount; currentStudent++ ) {
-                    printf("Enter the SCORE of the Student: ");
-                    scanf("%d", &score);
-                        while(score<0 && score > 100){
-                            printf("Invalid Score: Score should be from 0 to 100.\n");
-                            printf("Enter score again.\n");
-                            printf("Enter the SCORE of the Student: ");
-                            scanf("%d", &score);
-                        }
-                    if(score >= 90){strcpy("EXCELLENT", category);}
-                    else if(score >= 80){strcpy("VERY GOOD", category);}
-                    else if(score >= 75){strcpy("GOOD", category);}
-                    else if(score >= 60){strcpy("NEEDS IMPROVEMENT", category);}
-                                    else{strcpy("FAIL", category);}
-                    printf("Student %d\nCategory: %s ", currentStudent, category);
-            totalScore = score + totalScore;
-            if(currentStudent = 1){score = highScore; score = lowScore;}
-            else{if(score > highScore){highScore = score;} 
-                if(score < lowScore){lowScore=score;}}
-            if(score>=60){passCount++;}
-            else{failCount++;}
+                        printf("Enter the SCORE of the Student: ");
+                        scanf("%d", &score);
+                            while (score < 0 || score > 100)
+                            {   printf("Invalid Score: Score should be from 0 to 100.\n");
+                                printf("Enter score again.\n");
+                                printf("Enter the SCORE of the Student: ");
+                                scanf("%d", &score);
+                            }
+                            if(score >= 90){strcpy(category, "EXCELLENT");}
+                            else if(score >= 80){strcpy(category, "VERY GOOD");}
+                            else if(score >= 75){strcpy(category, "GOOD");}
+                            else if(score >= 60){strcpy(category, "NEEDS IMPROVEMENT");}
+                            else{strcpy(category, "FAIL");}
+                                printf("Student %d\nCategory: %s ", currentStudent);
+                        totalScore = score + totalScore;
+                        if(currentStudent = 1){score = highScore; score = lowScore;}
+                        else{if(score > highScore){highScore = score;} if(score < lowScore){lowScore=score;}}
+                        if(score>=60){passCount++;} else{failCount++;}
+                    }
+        aveScore = totalScore / studentCount;
 
-            aveScore = totalScore / studentCount;
+        printf("Highes Score: %d", highScore);
+        printf("Lowest Score: %d", lowScore);
+        printf("Total Score: %d", totalScore);
+        printf("Average Score: %d", aveScore);
+        printf("Passing Students: %d", passCount);
+        printf("Failing Students: %d", failCount);
 
-            print("Highes Score: %d", highScore);
-            print("Lowest Score: %d", lowScore);
-            print("Total Score: %d", totalScore);
-            print("Average Score: %d", aveScore);
-            print("Passing Students: %d", passCount);
-            print("Failing Students: %d", failCount);
-
-    }
-}       
+}
--- test.c
+++ test.c
@@ -1,20 +1,6 @@
 #include <stdio.h>
 
-int main(){
-    int studentCount, currentStudent, score, totalScore, highScore, lowScore,
-        aveScore, passCount, failCount;
-    char category[70];
+int main(){}
 
-        printf("Enter Number of Students: ");
-                scanf("%d", &studentCount);
-                    for(currentStudent = 1; currentStudent < studentCount; currentStudent++ ) {
-                    printf("Enter the SCORE of the Student: ");
-                    scanf("%d", &score);
-                        while(score<0 && score > 100){
-                            printf("Invalid Score: Score should be from 0 to 100.\n");
-                            printf("Enter score again.\n");
-                            printf("Enter the SCORE of the Student: ");
-                            scanf("%d", &score);
-                        }
-    }
-}
+
+
