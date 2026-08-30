7f137a93f2351c0a387d8936d274bf9055f1455e
seproispogi-lgtm <serpoispogi@email.com>
2026-08-30 10:09:33 +0800

    adding string regulation in getting the input of an integer

--- stud_perf.c
+++ stud_perf.c
@@ -9,13 +9,13 @@ int main(){
     char category[70];
         
         printf("Enter Number of Students: ");
-        scanf("%d", &studentCount);
-            while (studentCount <= 0){
+        while (scanf("%d", &studentCount) != 1 || studentCount <= 0){
                 printf("Invalid Input: Number should be greater than ZERO.\n"); 
                 printf("input valid number.\n");
+                while(getchar() != '\n');
                 printf("Enter Number of Students: ");
-                scanf("%d", &studentCount);
-            }
+        }
+
                 totalScore = 0;
                 highScore = 0;
                 lowScore = 0;
@@ -24,27 +24,33 @@ int main(){
         
         for(currentStudent = 0; currentStudent < studentCount; currentStudent++ ) {
                         printf("Enter the SCORE of the Student: ");
-                        scanf("%d", &score);
-                            while (score < 0 || score > 100)
-                            {   printf("Invalid Score: Score should be from 0 to 100.\n");
+                        while (scanf("%d", &score) != 1 || score < 0 || score > 100)
+                        {   
+                                printf("Invalid Score: Score should be from 0 to 100.\n");
                                 printf("Enter score again.\n");
+                                while(getchar() != '\n');
                                 printf("Enter the SCORE of the Student: ");
-                                scanf("%d", &score);
-                            }
+                        }
+
                             if(score >= 90){strcpy(category, "EXCELLENT");}
                             else if(score >= 80){strcpy(category, "VERY GOOD");}
                             else if(score >= 75){strcpy(category, "GOOD");}
                             else if(score >= 60){strcpy(category, "NEEDS IMPROVEMENT");}
                             else{strcpy(category, "FAIL");}
+
                                 printf("Student %d\nCategory: %s\n", currentStudent + 1, category);
+
                         totalScore = score + totalScore;
+
                         if(currentStudent == 0){highScore = score; lowScore = score;}
                         else{if(score > highScore){highScore = score;} if(score < lowScore){lowScore=score;}}
+
                         if(score>=60){passCount++;} else{failCount++;}
 
-
                     }
+
         aveScore = totalScore / studentCount;
+
         printf("\n===== RESULTS =====\n");
         printf("Highes Score: %d\n", highScore);
         printf("Lowest Score: %d\n", lowScore);
